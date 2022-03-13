8795b3855e20bc2287ed11ea619ac44853301279
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-03-13 12:58:54 +0200

    Update InventoryCountTests.java
    adding test cases:
    - TC01_verifyManagerCanNavigateToInventoryCount
    - TC02_verifyErrorMessageAppearsWhenSearchForInvalidInput
    - TC03_verifyResultsFoundWhenSearchForValidInput

--- src/tests/InventoryCountTests.java
+++ src/tests/InventoryCountTests.java
@@ -1,18 +1,39 @@
 package tests;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+import ui.InventoryCount.InventoryCount;
 import ui.InventoryCount.InventoryCountActions;
 import ui.InventoryCount.InventoryCountAssertions;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class InventoryCountTests {
 
+	InventoryCount page = new InventoryCount();
 	InventoryCountActions actions = new InventoryCountActions();
 	InventoryCountAssertions assertions = new InventoryCountAssertions();
 
 	@Test
-	public void verifyManagerCanNavigateToInventoryCount() {
+	public void TC01_verifyManagerCanNavigateToInventoryCount() {
 		actions.openNavigationMenu()//
 				.navigateToInventoryCount();
+
+		assertions.//
+				verifyInventoryCountIsOpened()//
+				.verifyInProgressTabIsSelected();
+	}
+
+	@Test
+	public void TC02_verifyErrorMessageAppearsWhenSearchForInvalidInput() {
+		actions.typeInSearchInput("abcdefg");
+		assertions.verifyNoResultsFoundIsDisplayed();
+	}
+
+	@Test
+	public void TC03_verifyResultsFoundWhenSearchForValidInput() {
+		actions.typeInSearchInput("Testing");
+		assertions.verifyInventoryListContainItems("Testing");
 	}
 }
